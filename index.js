4f20013a1d733c7b0da6c6e2ce5e6dcc1d23d4f7
Rahmat <oktrifianto@gmail.com>
2022-05-24 08:58:50 +0700

    Add route for create user

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app     = express();
 const port    = 3000 || process.env.SERVER_PORT;
 const lib     = require('./controllers/users.controllers');
+app.use(express.json()); // enable `req.body`
 
 app.get('/', (req, res) => {
   res.status(200).json({
@@ -13,6 +14,7 @@ app.get('/', (req, res) => {
 // Routes
 app.get('/users', lib.getUsers);
 app.get('/user/:id', lib.getUserById);
+app.post('/user', lib.createUser);
 
 app.listen(port, () => {
   console.log(`Your server running on port ${port}`);
