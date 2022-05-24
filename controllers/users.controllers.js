2e17d9e383f880b5580a1c61f3dfabe320c6e487
Rahmat <oktrifianto@gmail.com>
2022-05-24 08:01:25 +0700

    Create `getUserById` controllers

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -10,6 +10,23 @@ const getUsers = (req, res) => {
   })
 };
 
+// Get single user
+const getUserById = (req, res) => {
+  const { id } = req.params;
+  pool.query(`SELECT * FROM users WHERE id = ${id}`, (err, results) => {
+    if (err) throw err;
+    if (results.rows.length > 0) {
+      res.status(200).json(results.rows);
+    } else {
+      res.status(404).json({
+        status : res.statusCode,
+        message: 'User not found.'
+      });
+    }
+  });
+}
+
 module.exports = {
-  getUsers
+  getUsers,
+  getUserById
 };
